1a40c4e1d1ffeedec567dafa0911f665fdfdf373
isismynamee <koregawa101@gmail.com>
2023-04-18 14:31:10 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import { useCallback, useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState('')
+  const [bilanganGanjil, setBilanganGanjil] = useState(false)
+  const [bilanganPrima, setBilanganPrima] = useState(false)
+  const a = input.formInput?.length
+
+  const changeInput = useCallback(
+    (e) => {
+      const {name, value} = e.target
+
+      setInput((prev) => ({
+        ...prev,
+        [name]: value
+      }))
+    }, [setInput]
+  )
+
+  const submitInput = useCallback(
+    () => {
+      let hasil;
+      for(let i = 0; i < a; i++){
+        for(let d = 0; d <= i; d++){
+          hasil += `${input.formInput[i]}`
+          console.log(hasil)
+        }
+        console.log('\n')
+      }
+    }, [a]
+  )
+
+  const ganjil = useCallback(
+    () => {
+      if(input.formInput % 2 !== 0 ){
+        setBilanganGanjil(true)
+        console.log("Ganjil")
+      }
+    }, [input.formInput, setBilanganGanjil]
+  )
+  console.log(bilanganPrima, "prima")
+
+  const prima = useCallback(
+    () => {
+  let final = 1
+      for(let p = 0; p <= input.formInput; p++){
+        if(input.formInput % p === 0){
+          final = 2;
+          return final
+        }
+      }
+      console.log(final, "final")
+      if(final === 1 ){
+        setBilanganPrima(true)
+        console.log("prima")
+      }
+    }, [input.formInput]
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" name='formInput' onChange={changeInput} />
+      <button onClick={() => submitInput()}>Generate Segitiga</button>
+      <button onClick={ganjil}>Generate Ganjil</button>
+      <button onClick={prima}>Generate Prima</button>
+      <div>
+        {bilanganGanjil ? (
+          <>
+            <span>Ganjil</span>
+          </>
+        ): (
+          <>
+            <span>Genap</span>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
